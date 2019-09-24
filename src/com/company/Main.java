c8059f0dd9fdedd6d9ca6adfed52a7b25c95e5f7
mobile <mobile@itcube.win>
2019-09-24 18:34:40 +0300

    gg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    int a;
+    short b;
+    int c = 123_124_41;
+    long d = 56L;
+    byte e = 3;
+    float f = 2.5F;
+    double g = 67.5;
+    char h = 65;
+
     }
 }
